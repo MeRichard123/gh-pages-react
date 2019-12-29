a6aaf3e0dc1e9bec31382c7a8de06bf44c1201b4
MeRichard123 <rcoric03@gmail.com>
2019-12-29 17:41:51 +0000

    Updates

---
+++ precache-manifest.7e8bd6fc9d332f574ff0ea4c948a9950.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d063f773a544a9e65eea9c1c168ca40f",
+    "url": "/gh-pages-react/index.html"
+  },
+  {
+    "revision": "b4a0dcfb6ef190d5c768",
+    "url": "/gh-pages-react/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "46fb4ef9d68723fb6e09",
+    "url": "/gh-pages-react/static/js/2.e338b1be.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/gh-pages-react/static/js/2.e338b1be.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b4a0dcfb6ef190d5c768",
+    "url": "/gh-pages-react/static/js/main.a358bf34.chunk.js"
+  },
+  {
+    "revision": "ff8297da964405bd93d8",
+    "url": "/gh-pages-react/static/js/runtime-main.9dbc0111.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/gh-pages-react/static/media/logo.5d5d9eef.svg"
+  }
+]);
